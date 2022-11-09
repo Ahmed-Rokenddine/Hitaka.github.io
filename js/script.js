f0005959740b728bbc9c5c922e9a41cdb29b388f
Ahmed-Rokenddine <masteriko05@gmail.com>
2022-11-09 10:50:37 +0000

    website

--- js/script.js
+++ js/script.js
@@ -46,6 +46,95 @@ window.onscroll = () => {
      };
     
      
+     
+		function getValue() {
+        document.listForm.total.value = '';
+			var checkboxes =
+				document.getElementsByName('laptop');
+
+			var result = "";
+            var sum=0;
+            var sumfinal;
+            var x ;
+
+			
+            for (var i = 0; i < checkboxes.length; i++) {
+				if (checkboxes[i].value != 0) {
+                    x = checkboxes[i].placeholder;
+                    sumfinal = checkboxes[i].value  ;
+                    sum =  sum + parseInt(x*sumfinal)
+					
+                    console.log(i)
+                    document.listForm.total.value = sum;
+				}
+                
+			}
+					
+
+           
+		}
+        function showValue() {
+        document.platform.total.value = '';
+        var checkboxes =
+				document.getElementsByName('laptop');
+                var result = "";
+                for (var i = 0; i < checkboxes.length; i++) {
+				if (checkboxes[i].value != 0) {
+					result += checkboxes[i].value+ " x " + checkboxes[i].id
+                    +"\n";
+                    console.log(i)
+					
+				}
+                document.platform.total.value = result;	
+			}
+            
+                    
+                    }
+                    function foo() {   alert("Commande Validé!");
+   return true;
+}
+function increaseCount(a, b) {
+  var input = b.previousElementSibling;
+  var value = parseInt(input.value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  input.value = value;
+  getValue();showValue()
+}
+
+function decreaseCount(a, b) {
+  var input = b.nextElementSibling;
+  var value = parseInt(input.value, 10);
+  if (value > 0) {
+    value = isNaN(value) ? 0 : value;
+    value--;
+    input.value = value;
     
+  }
+  getValue();showValue()
+}
+function sidebar() {
+    var element = document.getElementById("nav-bar");
+    element.classList.toggle("navbar2");
+    var element = document.getElementById("closer");
+    element.classList.toggle("closed2");
+    var element = document.getElementById("side");
+    element.classList.toggle("btncloser");
+    
+ };
+ function closebar() {
+    var element = document.getElementById("nav-bar");
+    element.classList.remove("navbar2");
+    var element = document.getElementById("closer");
+    element.classList.remove("closed2");
+    var element = document.getElementById("side");
+     element.classList.remove("btncloser");
+    
+ };
+ function savecommande(){
+ var input = document.getElementById('saveServer').value;
+ localStorage.setItem('total', input);
+ }
+ 
  
     
